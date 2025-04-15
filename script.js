6a899d13a86a40ae5187f8c2d508a5c05d84b4ef
Pavel <yanushka.pavel@gmail.com>
2025-04-15 20:15:31 +0200

    Variables declaration

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("New text");
+
+const circle01 = document.querySelector(".circle-01");
+const circle02 = document.querySelector(".circle-02");
+const circle03 = document.querySelector(".circle-03");
+const circle04 = document.querySelector(".circle-04");
+const circle05 = document.querySelector(".circle-05");
+const circle06 = document.querySelector(".circle-06");
+
+const dash01 = document.querySelector(".dash-01");
+const dash02 = document.querySelector(".dash-02");
+const dash03 = document.querySelector(".dash-03");
+const dash04 = document.querySelector(".dash-04");
+const dash05 = document.querySelector(".dash-05");
+
+
+const image01 = document.querySelector(".image-decoration.first");
+const image02 = document.querySelector(".image-decoration.second");
+const image03 = document.querySelector(".image-decoration.third");
+const image04 = document.querySelector(".image-decoration.fourth");
+const image05 = document.querySelector(".image-decoration.fifth");
+

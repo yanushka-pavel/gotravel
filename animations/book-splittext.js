3928b801e02c28e02066f63469fd7037f1aa1091
yanushka-pavel <yanushka.pavel@gmail.com>
2025-07-06 21:14:06 +0200

    Added splitText animation for heading on book page

--- animations/book-splittext.js
+++ animations/book-splittext.js
@@ -1,8 +1,22 @@
 export function animateSplitText(target){
-    console.log("splittext animation works");
+    const splitTextTracker = document.querySelector('.section_book-about');
+    console.log("splittext animation works 2");
 const splitText = new SplitType(target, {
-    types: 'chars',
+    types: 'chars, words',
     tagName: 'span',
+    autoSplit: true,
 })
+gsap.to(splitText.chars,{
+    color:"#40583d",
+    stagger: 0.02,
+    scrollTrigger: {
+        trigger: splitTextTracker,
+        start: "bottom 10%",
+        end: "bottom 30%",
+        scrub: true,
+        once: false,
+    }
+},
+)
 
 }
